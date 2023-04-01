c046d279910486522a11c4a7b072281104330169
Hossein <h.dvd13@gmail.com>
2023-04-01 12:51:57 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (element) {
+        return element.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function (element) {
+        return element.startsWith(name);
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (element) {
+        return element.name === name;
+    });
+}
